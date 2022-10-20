5858c534e54a3a5c7fe37a5c91cd9007dc6f5936
kendrick Brown <chefken360@gmail.com>
2022-10-19 20:20:28 -0700

    npm install and some ccode

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // TODO: Include packages needed for this application
 const inquirer =require('inquirer')
 const fs = require('fs');
-
+const generateMarkdown = require(generateMarkdown);
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {name:"title",
+    message: "project title?",
+    type: "input"
+},{
+    name: "description",
+    message: "what did you do?",
+    type: "input"
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    fs.writeFile("filename.md",data,(err)=>{
+        
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((result) => {
+        const markDown = generateMarkdown(result);
+        writeToFile(markDown);
+    })
+}
 
 // Function call to initialize app
 init();
